e7c4793ef94467764c18165a4484b797352cefc5
Hongfei Ji <jihongfei@zepplabs.com>
2017-12-05 19:12:17 +0800

    finish xiniu downloader

--- python/xiniu_ipynb_downloader.py
+++ python/xiniu_ipynb_downloader.py
@@ -2,7 +2,9 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+For crawling python notebook from xiniu.
+Only support read from static htm page so far.
+
 """
 
 from bs4 import BeautifulSoup
@@ -11,7 +13,7 @@ import os
 import urllib2
 
 html_space = '/Users/admin/JavaProjects/DataWarehouseCourse/python/htm/'
-output_space = '/Users/admin/JavaProjects/DataWarehouseCourse/python/output'
+output_space = '/Users/admin/JavaProjects/DataWarehouseCourse/python/output/'
 
 #session = requests.Session()
 #payload = {'mobile': '13146810601', 'password': '810601', 'rd': '0.9470549339899308'}
@@ -31,38 +33,44 @@ def extract_ipynb_url(link):
     if e == -1:
         e = len(link)
     link = link[s: e]
-    link = url_unquote(link)
-    print(link)
-    return 'http://'+link[s: e]
+    link = url_unquote(str(link))
+    link = link.decode('utf-8')
+    return 'http://%s' % link
 
 def get_default_name(url):
     ext = '.ipynb'
-    s = link.rfind('/')
+    s = url.rfind('/')
     if s == -1:
         s = 0
-    e = link.rfind(ext)
+    e = url.rfind(ext)
     if e == -1:
         e = len(url)
-#    print('url:%s, s=%d ,e=%d' % (url, s, e))
-    name = url[s:e]
-#    print('before:'+name)
-#    print('after:'+name)
+    name = url[(s + 1) : e]
     return name + ext
 
 def url_unquote(encode_str):
     return urllib2.unquote(encode_str)
 
-def download(url):
+def download(dir_name, url):
     r = requests.get(url)
-    dname = get_default_name(url)
-    with open(dname, "wb") as f:
+    create_dir(os.path.join(output_space, dir_name))
+    default_name = get_default_name(url)
+    path = os.path.join(output_space, dir_name, default_name)
+    print('DOWNLOADING:\n%s\nTO PATH:\n%s\n' % (url, path))
+    with open(path, "wb") as f:
         f.write(r.content)
 
+def create_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    
 for f in os.listdir(html_space):
     htm = html_space + f
     soup = BeautifulSoup(open(htm), 'html.parser', from_encoding='utf-8')
     for a in soup.find_all("a", class_="am-text-truncate textunder"):
         link = a.get('href')
         url = extract_ipynb_url(link)
-#        download(url)
-        get_default_name(url)
+        dir_name = f
+        if dir_name.find('.htm') != -1:
+            dir_name = dir_name[0: dir_name.find('.htm')]
+        download(dir_name, url)
